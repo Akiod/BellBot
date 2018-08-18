d40e9a3c93f9b09ba5162e35dfaeedb4a44acef9
Akiod <admin@akiod.net>
2018-08-17 23:32:50 -0400

    implemented barebone support for word count tracker

---
+++ src/headers/words.h
@@ -0,0 +1,29 @@
+#ifndef _WORDS_H_
+#define _WORDS_H_
+
+#include "common.h"
+
+class WordUser
+{
+	public:
+	std::string username;
+	std::string channel;
+	std::string word;
+	unsigned int timestamp;
+
+	WordUser(std::string u, std::string c, std::string w, unsigned int t) : username(u), channel(c), word(w), timestamp(t) { }
+};
+
+class WordController
+{
+	std::vector<std::string> targetWords;
+	std::vector<WordUser> wordLogs;
+
+	public:
+	WordController();
+	~WordController();
+
+	void detect(std::string msg, std::string user, std::string channel);
+};
+
+#endif
---
+++ src/words.cpp
@@ -0,0 +1,14 @@
+#include "headers/words.h"
+
+WordController::WordController()
+{
+	
+}
+WordController::~WordController()
+{
+	
+}
+void WordController::detect(std::string msg, std::string user, std::string channel)
+{
+	
+}
